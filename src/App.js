57ff9ba4fa7c5f9138d1322c5a149c3490db9a98
Ho Ngoc Trung <trung0194@gmail.com>
2018-07-14 12:55:56 +0700

    Them router cho app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        chào bạn, bạn tên gì ))
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    constructor(props) {
+        super(props)
+        console.log('about:', props)
+    }
+
+    render() {
+        return (
+        <div>trang about</div>
+        );
+    }
+}
+  
+export default About
