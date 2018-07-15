ebb77afa7dfa2ea10cfaf3e745c1f619e290301d
Ho Ngoc Trung <trung0194@gmail.com>
2018-07-15 18:49:12 +0700

    Comment registerServiceWorker in index.js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as Constant from './constant/var'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import io from 'socket.io-client'
 window.socket = io(Constant.ioUrlConnect)
 // window.socket.on('connect', () => { console.log('--connect io') })
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
